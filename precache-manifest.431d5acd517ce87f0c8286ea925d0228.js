c2b6432b058972901bf524ef0c2f026bf2a69e9d
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-10 15:30:10 +0200

    Updates

---
+++ precache-manifest.431d5acd517ce87f0c8286ea925d0228.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0269c5aa93f12060689a3a9c50e10a76",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c5d9088860a92b1c4f4b",
+    "url": "./static/css/main.c023359f.chunk.css"
+  },
+  {
+    "revision": "d90c94d07a1f7893f775",
+    "url": "./static/js/2.f1ad0fc2.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "./static/js/2.f1ad0fc2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5d9088860a92b1c4f4b",
+    "url": "./static/js/main.f177371b.chunk.js"
+  },
+  {
+    "revision": "f69c79f773cd0f0e958e",
+    "url": "./static/js/runtime-main.3b8dd1f0.js"
+  },
+  {
+    "revision": "ff5c52693695b0ce55fbeadf7e193a53",
+    "url": "./static/media/Silom.ff5c5269.ttf"
+  },
+  {
+    "revision": "c853d8c3291611a0c3bba198a939b071",
+    "url": "./static/media/sprites.c853d8c3.png"
+  },
+  {
+    "revision": "2d6cbf09dd8de35e87023982d4501027",
+    "url": "./static/media/upheavtt.2d6cbf09.ttf"
+  }
+]);
